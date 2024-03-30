175817696d900b9e7cd03d9c26af746467af0812
Luiz Santos <luizfernando.santos900@gmail.com>
2024-03-30 18:09:50 -0300

    first commit

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,9 @@
+import "../styles/LoadingIndicator.css"
+
+const LoadingIndicator = () => {
+    return <div className="laoding-container">
+        <div className="loader"></div>
+    </div>
+}
+
+export default LoadingIndicator
